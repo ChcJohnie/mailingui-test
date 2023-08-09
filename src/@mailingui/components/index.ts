6d1c3e22be3d2fbfadbae7705f6e2a5bb583b7f2
Jan Shanel <shanel@webscope.io>
2023-08-09 15:35:26 +0200

    Install mailingui and deps

---
+++ src/@mailingui/components/index.ts
@@ -0,0 +1,8 @@
+export {Badge} from './badge/Badge'
+export {Button} from './button/Button'
+export {Divider} from './divider/Divider'
+export {Emoji} from './emoji/Emoji'
+export {HeroSection} from './heroSection/HeroSection'
+export {ListItem, ListItemContent, ListItemTitle, ListRoot} from './list/List'
+export {SocialIcon} from './socialIcon/SocialIcon'
+export {Text} from './text/Text'
